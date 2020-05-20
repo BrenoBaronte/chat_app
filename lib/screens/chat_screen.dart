4fcf7fb40d6470c11fc1b7a54f558a6be1633715
Breno Baronte <brenobaronte@hotmail.com>
2020-05-20 17:01:45 -0300

    Chat message heart icon

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -15,63 +15,76 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
 
   _buildMessage(Message message, bool isMe) {
+
+    final Container msg = Container(
+      margin: isMe
+          ? EdgeInsets.only(
+          top: 8.0,
+          bottom: 8.0,
+          left: 80.0)
+          : EdgeInsets.only(
+        top: 8.0,
+        bottom: 8.0,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      width: MediaQuery.of(context).size.width * 0.75,
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+        borderRadius: isMe
+            ? BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          bottomLeft: Radius.circular(15.0),
+        )
+            : BorderRadius.only(
+          topRight: Radius.circular(15.0),
+          bottomRight: Radius.circular(15.0),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+    if (isMe) {
+      return msg;
+    }
     return Row(
       children: <Widget>[
-        Container(
-          margin: isMe
-            ? EdgeInsets.only(
-                top: 8.0,
-                bottom: 8.0,
-                left: 80.0)
-            : EdgeInsets.only(
-                top: 8.0,
-                bottom: 8.0,
-            ),
-          padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
-          width: MediaQuery.of(context).size.width * 0.75,
-          decoration: BoxDecoration(
-            color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
-            borderRadius: isMe
-              ? BorderRadius.only(
-              topLeft: Radius.circular(15.0),
-              bottomLeft: Radius.circular(15.0),
-                )
-              : BorderRadius.only(
-              topRight: Radius.circular(15.0),
-              bottomRight: Radius.circular(15.0),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                message.time,
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 8.0),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-        ),
+        msg,
         IconButton(
-          icon: Icon(Icons.more_horiz),
+          icon: message.isLiked
+            ? Icon(Icons.favorite)
+            : Icon(Icons.favorite_border),
           iconSize: 30.0,
-          color: Colors.black,
+          color: message.isLiked
+            ? Theme.of(context).primaryColor
+            : Colors.blueGrey,
           onPressed: () {},
-        )
+        ),
       ],
     );
+  }
+
+  _buildMessageComposer() {
+    return Text('hello');
   }
 
   @override
@@ -128,6 +141,7 @@ class _ChatScreenState extends State<ChatScreen> {
               ),
             ),
           ),
+          _buildMessageComposer(),
         ],
       ),
     );
