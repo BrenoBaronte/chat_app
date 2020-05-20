dcbf70acae48eef11e9f24bb4092964e3185ade9
Breno Baronte <brenobaronte@hotmail.com>
2020-05-20 16:51:22 -0300

    chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,6 +13,67 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  _buildMessage(Message message, bool isMe) {
+    return Row(
+      children: <Widget>[
+        Container(
+          margin: isMe
+            ? EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+                left: 80.0)
+            : EdgeInsets.only(
+                top: 8.0,
+                bottom: 8.0,
+            ),
+          padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+          width: MediaQuery.of(context).size.width * 0.75,
+          decoration: BoxDecoration(
+            color: isMe ? Theme.of(context).accentColor : Color(0xFFFFEFEE),
+            borderRadius: isMe
+              ? BorderRadius.only(
+              topLeft: Radius.circular(15.0),
+              bottomLeft: Radius.circular(15.0),
+                )
+              : BorderRadius.only(
+              topRight: Radius.circular(15.0),
+              bottomRight: Radius.circular(15.0),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                message.time,
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                message.text,
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.more_horiz),
+          iconSize: 30.0,
+          color: Colors.black,
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,9 +116,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   topLeft: Radius.circular(30.0),
                 ),
                 child: ListView.builder(
+                  reverse: true,
+                  padding: EdgeInsets.only(top: 15.0),
                   itemCount: messages.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Text(messages[index].text);
+                    final Message message = messages[index];
+                    final bool isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
                   },
                 ),
               ),
